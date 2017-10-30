5f25307d947313ef8293e313d9b8b0e223a66a59
teresa-carmona <31965190+teresa-carmona@users.noreply.github.com>
2017-10-30 16:01:23 +0100

    add GetMapping annotation and log

--- src/main/java/com/lab4/subject/SubjectController.java
+++ src/main/java/com/lab4/subject/SubjectController.java
@@ -1,15 +1,19 @@
 package com.lab4.subject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SubjectController {
 
     @Value("${words}")
     private String words;
 
+    @GetMapping("/")
     public @ResponseBody String getWord() {
         String[] wordArray = words.split(",");
         int i = (int)Math.round(Math.random() * (wordArray.length - 1));
